064368ed201d7991543ba0b018ea1aa5cf93e3d0
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-03 12:04:31 +0200

    first commit

---
+++ neore-shop/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1>Neore Shop</h1>
+        <p>Welcome to Neore Shop</p>
+        <button>Shop Now</button>
+      </div>
+    </>
+  )
+}
+
+export default App
