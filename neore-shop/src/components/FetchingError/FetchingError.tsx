cce442cdd9e7cff37a15a27f9133e814d3fac147
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-10 17:24:55 +0200

    fix(CardsContainercss,Homepage.css) improve UI experience

--- neore-shop/src/components/FetchingError/FetchingError.tsx
+++ neore-shop/src/components/FetchingError/FetchingError.tsx
@@ -1,10 +1,10 @@
 export default function FetchingError() {
-    return (
-        <div className="fetching-error-container">
-            <div className="fetching-error-content">
-            <h1>Oops! Something went wrong</h1>
-            <p>We couldn&apos;t load the data. Please try again later.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="fetching-error-container">
+      <div className="fetching-error-content">
+        <h1>Oops! Something went wrong</h1>
+        <p>We couldn&apos;t load the data. Please try again later.</p>
+      </div>
+    </div>
+  );
 }
