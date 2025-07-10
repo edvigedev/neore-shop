f91896ca77fe6c51f74fa9fda5bb69778bb202ad
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-10 16:35:50 +0200

    feat(card.tsx) implement card component

---
+++ neore-shop/src/components/Card/Card.tsx
@@ -0,0 +1,30 @@
+import './Card.css';
+
+interface Product {
+  thumbnail: string;
+  title: string;
+  price: number;
+  discountPercentage: number;
+  description: string;
+}
+
+export default function Card({ product }: { product: Product }) {
+  return (
+    <div className="card">
+      <div className="card-image-section">
+        <img src={product.thumbnail} alt={product.title} />
+        <a href={`/cart`} className="add-to-cart">
+          +
+        </a>
+      </div>
+      <div className="card-info">
+        <h3>{product.title}</h3>
+        <div className="card-price">
+          <h3>€{product.price}</h3>
+          <h3>-{Math.round(product.discountPercentage)}%</h3>
+        </div>
+        <p className="card-description">{product.description.substring(0, 50)}...</p>
+      </div>
+    </div>
+  );
+}
