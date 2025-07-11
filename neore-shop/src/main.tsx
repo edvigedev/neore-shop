63d3f9ad0c1afb54fb31427a86c8abc05ee7ac6e
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-11 17:14:27 +0200

    fix(main.tsx) adjust typo in the basename path

--- neore-shop/src/main.tsx
+++ neore-shop/src/main.tsx
@@ -7,7 +7,7 @@ import ProductDetails from './pages/ProductDetails/ProductDetails.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/neore-shop">
+    <BrowserRouter basename="/neore-shop/">
       <Routes>
         <Route index element={<App />} />
         <Route path="/products/:id" element={<ProductDetails />} />
