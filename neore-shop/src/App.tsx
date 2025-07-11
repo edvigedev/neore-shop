1e48e09d5af35dbd5b271e7de1fb24b7c24e4a47
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-11 17:14:02 +0200

    feat(NavBar.tsx) adjust the Navbar and add the Favorites link, no page yet

--- neore-shop/src/App.tsx
+++ neore-shop/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from './pages/Homepage/HomePage';
+import NavBar from './components/Navbar/NavBar';
 
 function App() {
   return (
     <>
       <div className="container">
+        <NavBar />
         <HomePage />
       </div>
     </>
--- neore-shop/src/components/Navbar/NavBar.tsx
+++ neore-shop/src/components/Navbar/NavBar.tsx
@@ -1,13 +1,21 @@
 import './NavBar.css';
 import cartImage from '../../assets/cart.png';
+import { Link } from 'react-router';
 
 export default function NavBar() {
   return (
     <nav className="nav-bar">
-      <div className="neore-logo"> Neore</div>
+      <Link to="/" className="no-underline-link">
+        <div className="neore-logo"> Neore</div>
+      </Link>
       <ul className="nav-bar-links">
         <li>My Profile</li>
         <li>Logout</li>
+        <li>
+          <Link to="/favorites" className="no-underline-link">
+            Favorites
+          </Link>
+        </li>
         <li>
           <button className="cart-button">
             <img src={cartImage} alt="Cart" />
