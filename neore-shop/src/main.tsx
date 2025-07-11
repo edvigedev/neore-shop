608d7b938f1c046709c31e7fc2b391b724099f5e
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-07-11 16:59:52 +0200

    feat(main.tsx) add BrowserRouter and Routes

--- neore-shop/src/main.tsx
+++ neore-shop/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 import App from './App.tsx';
+import ProductDetails from './pages/ProductDetails/ProductDetails.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/neore-shop">
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
