46fa0a94122522dfeec755c1e761548370c6a9eb
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-08-20 11:22:17 +0200

    fix(AdminGuard) removed unnecessay Loading state

--- neore-shop/src/components/ProtectedRoute/AdminGuard.tsx
+++ neore-shop/src/components/ProtectedRoute/AdminGuard.tsx
@@ -1,21 +1,15 @@
 import { useAuth } from '../../context/AuthContext/AuthContext';
 import { Navigate } from 'react-router-dom';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 export default function AdminGuard({ children }: { children: React.ReactNode }) {
   const { token, user } = useAuth();
-  // If we don't have the user object yet but we have a token,
-  if (token && !user) {
-    return <LoadingSpinner />;
-  }
 
-  // If we don't have a token, redirect to login
-  if (!token) {
+  // Now that loading is finished, we can safely check the final data.
+  if (!user || !token) {
     return <Navigate to="/login" replace />;
   }
 
-  // If the user is not an admin, redirect to home
-  if (user?.role !== 'admin') {
+  if (user.role !== 'admin') {
     return <Navigate to="/" replace />;
   }
 
