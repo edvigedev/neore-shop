bb96df93163c8d88a9a4ae6effed757414352813
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-08-22 16:22:13 +0200

    feat(playwright) add data-testid for FetchingError

--- neore-shop/src/components/FetchingError/FetchingError.tsx
+++ neore-shop/src/components/FetchingError/FetchingError.tsx
@@ -2,11 +2,15 @@ import './FetchingError.css';
 
 export default function FetchingError() {
   return (
-    <div className="fetching-error-container">
-      <div className="fetching-error-content">
-        <h1>Oops! Something went wrong</h1>
-        <p>We couldn&apos;t load the data. Please try again.</p>
-        <button onClick={() => window.location.reload()}>Try again</button>
+    <div className="fetching-error-container" data-testid="fetching-error">
+      <div className="fetching-error-content" data-testid="fetching-error-content">
+        <h1 data-testid="fetching-error-title">Oops! Something went wrong</h1>
+        <p data-testid="fetching-error-message">
+          We couldn&apos;t load the data. Please try again.
+        </p>
+        <button onClick={() => window.location.reload()} data-testid="fetching-error-retry-button">
+          Try again
+        </button>
       </div>
     </div>
   );
